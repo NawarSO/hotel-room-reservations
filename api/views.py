904ea6cd9bab95c2a4c36a1d8e3880f2eb635356
Lana_alhalabi <www.lanaalhalabi666@gmail.com>
2025-07-08 00:01:35 +0300

    the SrS and views

--- api/views.py
+++ api/views.py
@@ -1,5 +1,54 @@
 from django.shortcuts import render
 from rest_framework import viewsets, filters
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
-
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from .models import *
+from .serializer import *
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]   # IsAdminUser  
+
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['number']
+    permission_classes = [AllowAny] #IsAuthenticated
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    permission_classes = [AllowAny] #IsAuthenticated
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [AllowAny] #IsAuthenticated
+
+class InvoiceViewSet(viewsets.ModelViewSet):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
+    permission_classes = [AllowAny] #IsAuthenticated
+
+class OfferViewSet(viewsets.ModelViewSet):
+    queryset = Offer.objects.all()
+    serializer_class = OfferSerializer
+    permission_classes = [AllowAny]   # employees only
+
+class ComplaintViewSet(viewsets.ModelViewSet):
+    queryset = Complaint.objects.all()
+    serializer_class = ComplaintSerializer
+    permission_classes = [AllowAny]
+
+class ResponseViewSet(viewsets.ModelViewSet):
+    queryset = Response.objects.all()
+    serializer_class = ResponseSerializer
+    permission_classes = [AllowAny]   # IsAdminUser
+
+class ReportViewSet(viewsets.ModelViewSet):
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
+    permission_classes = [AllowAny] # IsAdminUser
